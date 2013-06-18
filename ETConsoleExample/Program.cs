6c97aea2def15022c51bbc26a16332911d7b49e2
Michael Clark <michaelallenclark@gmail.com>
2013-06-18 14:31:15 -0400

    Added all files

---
+++ ETConsoleExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using ETConsoleExample.ExactTargetSOAPAPI;
+
+
+namespace ETConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.Name = "UserNameSoapBinding";
+            binding.Security.Mode = BasicHttpSecurityMode.TransportWithMessageCredential;
+            binding.Security.Message.ClientCredentialType = BasicHttpMessageCredentialType.UserName;
+            binding.ReceiveTimeout = new TimeSpan(0, 5, 0);
+            binding.OpenTimeout = new TimeSpan(0, 5, 0);
+            binding.CloseTimeout = new TimeSpan(0, 5, 0);
+            binding.SendTimeout = new TimeSpan(0, 5, 0);
+
+            EndpointAddress endpoint = new EndpointAddress("https://webservice.exacttarget.com/Service.asmx");
+            SoapClient ETSOAPAPI = new SoapClient(binding, endpoint);
+
+            ETSOAPAPI.ClientCredentials.UserName.UserName = "ccc";
+            ETSOAPAPI.ClientCredentials.UserName.Password = "ccc";
+
+            RetrieveRequest getLists = new RetrieveRequest();
+            getLists.ObjectType = "List";
+            getLists.Properties = new string[] { "ID", "ListName" };
+
+            String RequestID = String.Empty;
+            APIObject[] responseObjects = new APIObject[] { };
+            String OverallStatus = String.Empty;
+
+            OverallStatus = ETSOAPAPI.Retrieve(getLists, out RequestID, out responseObjects);
+
+            Console.WriteLine("OverallStatus: " + OverallStatus + "  RequestID: " + RequestID);
+
+            if (responseObjects.Length > 0)
+            {
+                foreach (List l in responseObjects)
+                {
+                    Console.WriteLine("ListID : " + l.ID.ToString() + " Name: " + l.ListName);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 ETConsoleExample
 ================
 
-ETConsoleExample
+This console application illustrates the bare minimum setup required in order to access ExactTarget's SOAP API. 
+
+## Requirements ##
+- Visual Studio 2010 or 2012
+
+## Getting Started ##
+- Open the ETConsoleExample.sln file
+- Open the Program.cs file
+- On line 24, verify that the endpoint address is correct for your account.  This value will need to be one of the following:
+ -  https://webservice.exacttarget.com/Service.asmx
+ -  https://webservice.s4.exacttarget.com/Service.asmx
+ -  https://webservice.s6.exacttarget.com/Service.asmx
+ -  If you are not sure of the correct value for your account, please contact ExactTarget Support
+- On line 27, replace 'ccc' with your username
+- On line 28, replace 'ccc' with your password
+- Run the application in debug
